50200ff9673fdb93dcd2052a2abdfcead3e827c1
yuunh <abcdef1470@naver.com>
2023-04-25 15:03:20 +0900

    react
    react

--- 리액트/shop/src/App.js
+++ 리액트/shop/src/App.js
@@ -4,48 +4,94 @@ import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import { useState } from 'react';
 // import bg from './img/bg.png';
 import data from './data.js';
+import { Routes, Route, Link, useNavigate, Outlet, Navigate} from 'react-router-dom';
+import Detail from './routes/detail';
 
 function App() {
 
   let [shoes] = useState(data);
   console.log(shoes);
 
+  let navigate = useNavigate(); // 페이지 이동 도와주는 함수
+
   return (
     <div className="App">
+
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={() => { navigate(-1)}}>Home</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/detail')}}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      {/* <div className='main-bg' style={{ backgroundImage : 'url(' + bg + ')'}}> */}
-      <div className='main-bg'></div>
+      <Link to='/'>홈</Link>
+      <Link to='/detail'>상세페이지</Link>
 
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-4'>
-            <img src="https://yuunh.github.io/shop/shoes1.jpg" width="80%"/>
-            <h4>{ shoes[0].title }</h4>
-            <p>{ shoes[0].price }</p>
+      <Routes>
+        <Route path='/' element={
+          <>
+          {/* <div className='main-bg' style={{ backgroundImage : 'url(' + bg + ')'}}> */}
+          <div className='main-bg'></div>
+          <div className='container'>
+            <div className='row'>
+            {/* <Card shoes={shoes[0]} i={1}></Card>
+                <Card shoes={shoes[1]} i={2}></Card>
+                <Card shoes={shoes[2]} i={3}></Card> */}
+              {
+                shoes.map((a, i) => {
+                  return(
+                    <Card shoes={shoes[i]} i={i + 1}></Card>
+                  )
+                })
+              }
+            </div>
           </div>
-          <div className='col-md-4'>
-            <img src="https://yuunh.github.io/shop/shoes2.jpg" width="80%"/>
-            <h4>{ shoes[1].title }</h4>
-            <p>{ shoes[1].price }</p>
-          </div>
-          <div className='col-md-4'>
-            <img src="https://yuunh.github.io/shop/shoes3.jpg" width="80%"/>
-            <h4>{ shoes[2].title }</h4>
-            <p>{ shoes[2].price }</p>
-          </div>
-        </div>
-      </div>
+          </>
+        }/>
+        <Route path='/detail' element={<Detail></Detail>}/>
+        <Route path='/about' element={<About></About>}>
+          <Route path='member' element={<div>멤버임</div>}/>
+          <Route path='location' element={<div>멤버아님</div>}/>
+        </Route>
+        <Route path='/event' element={<Event></Event>}>
+          <Route path='one' element={<div>첫 주문시 왕배추즙 서비스!</div>}/>
+          <Route path='two' element={<div>생일기념 쿠폰받기!</div>}/>
+        </Route>
+      </Routes>
+
     </div>
   );
+}
+
+function Card(props) {
+  return(
+    <div className='col-md-4'>
+      <img src={"https://yuunh.github.io/shop/shoes" + props.i + ".jpg"} width="80%"/>
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.price }</p>
+    </div>
+  )
+}
+
+function About() {
+  return(
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Event() {
+  return(
+    <div>
+      <h2>오늘의 이벤트</h2>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 export default App;
